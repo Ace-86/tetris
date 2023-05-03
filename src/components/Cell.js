1baabffd1f481e6ac4b3e47659af64d9b5bf3866
Ace <kronicrespawn@gmail.com>
2023-05-03 09:49:34 -0500

    styles updated; unsolved errors

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { GameObject } from '../gameObject';
 
-function Cell({ type }) {
-  return (
-    <div>
+
+const Cell = ({ type }) => (
+
+    <StyledCell type={'L'} color={GameObject['L'].color}>
       cell
-    </div>
-  );
-}
+    </StyledCell>
+  )
+
 
 export default Cell;
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -3,19 +3,24 @@ import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 import { createStage } from '../gameHelpers';
+import { StyledTetrisWrapper, StyledTetris} from './styles/StyledTetris';
 
-function Tetris() {
+
+const Tetris = () => {
   return (
-    <div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
       <Stage stage={createStage()}/>
-      <aside>
-        <div>
-        <Display text="Score" />
-        <Display text="Rows" />
-        <Display text="Level" />
-        </div>
-      </aside>
-    </div>
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 }
 
