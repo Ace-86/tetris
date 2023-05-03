c52b1e8e7928a5f3ed4708f8c8dd0f5decea2920
Ace <kronicrespawn@gmail.com>
2023-05-02 20:36:01 -0500

    scaffolded main components

---
+++ src/components/StartButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StartButton({callback}) {
+  return (
+    <div>
+      START GAME
+    </div>
+  );
+}
+
+export default StartButton;
---
+++ src/components/Tetris.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+
+function Tetris() {
+  return (
+    <div>
+      <Stage />
+      <aside>
+        <div>
+        <Display text="Score" />
+        <Display text="Rows" />
+        <Display text="Level" />
+        </div>
+      </aside>
+    </div>
+  );
+}
+
+export default Tetris;
