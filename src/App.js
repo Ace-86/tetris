89c8d78951df8f61f90350b66b960d11c05232d9
Ace <kronicrespawn@gmail.com>
2023-05-04 13:16:59 -0500

    restructured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
-import Tetris from './components/Tetris'
+// import React from "react";
+// import Tetris from './components/Tetris'
+import "./styles.css"
+import  Game  from "./components/Game";
 
 function App() {
   return (
     <div className="App">
-      <Tetris/> 
+      <h1>Tetris Game</h1> 
+      <Game rows={20} columns={10} />
+        
     </div>
   );
 }
