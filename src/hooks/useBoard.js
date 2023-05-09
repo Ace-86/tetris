d29e5d8399cc08f418e313dc852f664a7dae7873
Ace <kronicrespawn@gmail.com>
2023-05-09 09:25:52 -0500

    hooks added

--- src/hooks/useBoard.js
+++ src/hooks/useBoard.js
@@ -1,7 +1,25 @@
-import { useState } from "react";
-import {buildBoard} from "../utilities/BuildBoard";
+import { useState, useEffect } from "react";
+import {buildBoard, nextBoard} from "../utilities/BuildBoard";
 
-export const useBoard = ({ rows, columns}) => {
-    const [board, setBoard] = useState(buildBoard({rows, columns}))
+export const useBoard = ({ 
+    rows, 
+    columns, 
+    player, 
+    resetPlayer, 
+    addLinesCleared 
+}) => {
+    const [board, setBoard] = useState(buildBoard({rows, columns}));
+
+    useEffect(() => {
+        setBoard((previousBoard) =>
+        nextBoard({
+            board: previousBoard,
+            player,
+            resetPlayer,
+            addLinesCleared
+        })
+        )
+    }, [player, resetPlayer, addLinesCleared]);
+
     return [board];
 }
