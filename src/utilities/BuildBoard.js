4cdd5a1a600185089d5e999e31a9b14786909dcb
Ace <kronicrespawn@gmail.com>
2023-05-10 10:16:10 -0500

    coliision and rotation logic added

--- src/utilities/BuildBoard.js
+++ src/utilities/BuildBoard.js
@@ -37,7 +37,44 @@ export const nextBoard = ({
   return {
     rows,
     size: { ...board.size}
+  };
+};
+
+export const hasCollision = ({ board, position, shape}) => {
+  for (let y= 0; y < shape.length; y++) {
+    const row = y + position.row;
+
+    for(let x = 0; x < shape[y].length; x++) {
+      if (shape[y][x]) {
+        const column = x + position.column;
+
+        if (
+          board.rows[row] &&
+          board.rows[row][column] &&
+          board.rows[row][column].occupied
+        ) {
+          return true;
+        }
+      }
+    }
   }
+  return false; 
 }
 
+export const isWithinBoard = ({ board, position, shape}) => {
+  for (let y = 0; y < shape.length; y++) {
+    const row = y + position.row;
+
+    for (let x = 0; x < shape[y].length; x++) {
+      if (shape[y][x]) {
+        const column = x + position.column;
+        const isValidRotation = board.row[row] && board.rows[row][column];
+      
+      if (!isValidRotation) return false;
+      }
+    }
+  }
+
+  return true;
+}
 
