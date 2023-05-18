eeb33b6749840075a21056bd436b81ef261e5b05
Ace <kronicrespawn@gmail.com>
2023-05-18 08:57:00 -0500

    video setting looped

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 // import Tetris from './components/Tetris'
 import "./styles.css"
 import  Game  from "./components/Game";
+// import "./components/styles/Video.css"
 
 function App() {
   return (
     <div className="App">
-      {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
+      <video src='/videos/VideoSpace.mp4' autoPlay loop muted />
       <h1>Tetris Game</h1> 
       <Game rows={20} columns={10} />
         
