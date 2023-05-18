cd4ceae72abc2b79fc5467847330d2da872cda02
Ace <kronicrespawn@gmail.com>
2023-05-18 07:59:50 -0500

    video files added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import  Game  from "./components/Game";
 function App() {
   return (
     <div className="App">
+      {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
       <h1>Tetris Game</h1> 
       <Game rows={20} columns={10} />
         
